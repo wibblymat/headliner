1451b2a99310307f4383951417fae546290f8ba8
Mat Scales <mat@wibbly.org.uk>
2012-11-23 19:56:55 +0000

    Made things work on Twitter
    Since I think it's the bbc.in links on twitter that are the main
    problem, lets make it work there!

--- headliner.js
+++ headliner.js
@@ -2,7 +2,9 @@ function changeLink(link)
 {
 	"use strict";
 	var request = new XMLHttpRequest();
-	request.open("GET", link.href);
+	var url = link.getAttribute("data-expanded-url") || link.href;
+	url.replace(/^https?/, "");
+	request.open("GET", url);
 	request.responseType = "document";
 	request.onreadystatechange = function()
 	{
@@ -14,7 +16,7 @@ function changeLink(link)
 	request.send();
 }
 
-var links = document.querySelectorAll("a[href*='http://bbc.in/']");
+var links = document.querySelectorAll("a[href*='//bbc.in/'], a[href*='//t.co/'][data-expanded-url*='//bbc.in/']");
 
 for(var i = 0; i < links.length; i++)
 {
